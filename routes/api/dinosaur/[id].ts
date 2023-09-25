02e9ea2a76135f51a542aa42c9fd607b4e79a07d
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-09-24 18:07:39 -0600

    feat: Agregado put y delete de ejemplo

--- routes/api/dinosaur/[id].ts
+++ routes/api/dinosaur/[id].ts
@@ -1,17 +1,29 @@
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 import prismaClient from "@/database/prisma.ts";
-import { Dinosaur } from "@/generated/client/index.d.ts";
+import { Dinosaur, Prisma } from "@/generated/client/deno/edge.ts";
 
 export const handler: Handlers = {
   async GET(_req: Request, ctx: HandlerContext) {
     const { id } = ctx.params;
-    const dinosaur: Dinosaur = await prismaClient.dinosaur.findFirst(
+    const dinosaur: Dinosaur | null = await prismaClient.dinosaur.findFirst(
       {
         where: {
           id: Number(id),
         },
       },
     );
+
+    if (dinosaur === null) {
+      return new Response(
+        JSON.stringify({
+          data: dinosaur,
+          message: "El elemento no fue encontrado exitosamente.",
+        }),
+        {
+          status: 404,
+        },
+      );
+    }
 
     return new Response(
       JSON.stringify({
@@ -30,7 +42,27 @@ export const handler: Handlers = {
     });
 
     return new Response(
-      JSON.stringify({}),
+      JSON.stringify({
+        message: "El elemento fue eliminado exitosamente.",
+      }),
+    );
+  },
+  async PUT(req: Request, ctx: HandlerContext) {
+    const { id } = ctx.params;
+    const body = (await req.json()) as Prisma.DinosaurCreateInput;
+
+    await prismaClient.dinosaur.update({
+      where: {
+        id: Number(id),
+      },
+      data: body,
+    });
+
+    return new Response(
+      JSON.stringify({
+        data: body,
+        message: "El elemento fue actualizado exitosamente.",
+      }),
     );
   },
 };
