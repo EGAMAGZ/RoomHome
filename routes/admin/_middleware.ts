51937fe84c38db299536915f286b8fe8565b5e29
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-10-05 14:07:19 -0600

    fix: Arreglada y simplificada validacion de si usuario es empleado

--- routes/admin/_middleware.ts
+++ routes/admin/_middleware.ts
@@ -3,11 +3,9 @@ import SessionState from "@/model/session.ts";
 import { getCookies } from "$cookies";
 import {
   ADMIN_LOGIN_URL,
-  ADMIN_LOGOUT_URL,
   ADMIN_ROOT_URL,
   ROOT_URL,
 } from "@/utils/config.ts";
-import { verifyJWT } from "@/utils/jwt.ts";
 
 export async function handler(
   req: Request,
@@ -19,37 +17,13 @@ export async function handler(
   const { userSession } = getCookies(req.headers);
   console.log(`Admin middleware - User Session: ${userSession}`);
 
-  // Usuario no ha iniciado sesion
-  // if (userSession === undefined) {
-  //   if (url.pathname === ADMIN_LOGIN_URL) {
-  //     return await ctx.next();
-  //   }
-  //   const headers = new Headers(req.headers);
-  //   headers.append("Location", ADMIN_LOGIN_URL);
-
-  //   return new Response(null, {
-  //     status: 307,
-  //     headers,
-  //   });
-  // }
-  if (userSession === undefined) {
-    
+  // Acceso exclusivo admin login al no haber iniciado sesion
+  if (userSession === undefined && url.pathname === ADMIN_LOGIN_URL) {
     return await ctx.next();
   }
-  try {
-    console.log("PAYLOAD");
-    const { payload, protectedHeader } = await verifyJWT(userSession);
-    if (!payload.empleado) {
-      console.log("NO EMPLEADO");
 
-      return new Response(null, {
-        status: 303,
-        headers: {
-          "Location": ROOT_URL,
-        },
-      });
-    }
-  } catch (_error) {
+  // Redireccion a raiz si no es admin
+  if (!ctx.state.isEmployee) {
     return new Response(null, {
       status: 303,
       headers: {
@@ -57,7 +31,8 @@ export async function handler(
       },
     });
   }
-  // Usuario ha iniciado sesion
+
+  // Redireccion a raiz de admin en caso de haber inicado sesion
   if (url.pathname === ADMIN_LOGIN_URL) {
     const headers = new Headers(req.headers);
     headers.append("Location", ADMIN_ROOT_URL);
