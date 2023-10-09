dbf5ace9ee407e785e238ac1223aaff51d34d107
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-10-09 07:49:09 -0600

    feat(Header): Creado Header para diferenciarse del Hero (#25)

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/Header.tsx";
+import Hero from "../components/Hero.tsx";
 import { PageProps } from "$fresh/server.ts";
 import SessionState from "@/model/session-state.ts";
 
 export default function Home(props: PageProps<any, SessionState>) {
   return (
     <>
-      <Header
+      <Hero
         imgUrl="/img/guillaume_meurice.jpg"
         isLoggedIn={props.state.isLoggedIn}
       />
