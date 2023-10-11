3a25b3da3329a87678aa87f39853420ff94109ab
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-10-10 20:18:34 -0600

    feat(Login)!: Mejorado inputs para el login

---
+++ islands/Input.tsx
@@ -0,0 +1,61 @@
+import { Signal, useComputed, useSignal } from "@preact/signals";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
+interface InputProps {
+  value: Signal<string>;
+  error: Signal<string>;
+  label: string;
+  type: "email" | "number" | "password" | "text";
+  name: string;
+  required?: boolean;
+  disabled?: boolean;
+  classList?: string;
+}
+
+export function Input(props: InputProps) {
+  const isTouched = useSignal(false);
+
+  const isEmpty = useComputed(() => props.value.value.trim() === "");
+
+  const showErrors = useComputed(() =>
+    isTouched.value && props.error.value && !isEmpty.value
+  );
+
+  const handleFocus = () => {
+    isTouched.value = true;
+  };
+
+  const handleBlur = () => {
+    isTouched.value = true;
+  };
+
+  function handleInput(event: Event) {
+    props.value.value = (event.target as HTMLInputElement).value;
+  }
+
+  return (
+    <div class={`form-control ${props.classList ?? ""}`}>
+      <label class="label">
+        <span class="label-text font-semibold">{props.label}</span>
+      </label>
+      <input
+        type={props.type}
+        name={props.name}
+        value={props.value}
+        required={props.required ?? false}
+        disabled={!IS_BROWSER || props.disabled}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onInput={handleInput}
+        class={`input input-bordered ${
+          showErrors.value ? "input-error" : "input-primary"
+        }`}
+      />
+      <label class="label">
+        <span class="label-text text-error">
+          {showErrors.value && props.error.value}
+        </span>
+      </label>
+    </div>
+  );
+}
