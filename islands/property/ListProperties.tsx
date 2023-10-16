0d0af7109e8975965f23ff24262771f1dfc95c60
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-10-16 11:45:06 -0600

    fix(CU-013)!: Solucionado el consultar inmuebles

--- islands/property/ListProperties.tsx
+++ islands/property/ListProperties.tsx
@@ -1,4 +1,4 @@
-import { useSignal } from "@preact/signals";
+import { useComputed, useSignal } from "@preact/signals";
 import { ApiResponse } from "@/schema/api-response.ts";
 import { InmueblesAlquilerWithPropietary } from "@/schema/property.ts";
 import Button from "@/components/Button.tsx";
@@ -6,11 +6,10 @@ import { PropertyTable } from "@/components/property/PropertyTable.tsx";
 
 interface ListPropertiesProps {
   propertiesList: InmueblesAlquilerWithPropietary[];
-  origin: string;
 }
 
 export default function ListProperties(
-  { propertiesList, origin }: ListPropertiesProps,
+  { propertiesList }: ListPropertiesProps,
 ) {
   const properties = useSignal<InmueblesAlquilerWithPropietary[]>(
     propertiesList,
@@ -19,21 +18,26 @@ export default function ListProperties(
   const isLoading = useSignal(false);
   const isMaxElements = useSignal(false);
 
+  const showButton = useComputed(() =>
+    properties.value.length >= 10 && !(isMaxElements.value)
+  );
+
   function handleClick() {
     const loadProperties = async () => {
-      const url = new URL(`${origin}/api/property`);
-      url.searchParams.append("skip", String(skip.value));
+      const searchParams = new URLSearchParams();
+      searchParams.append("skip", String(skip.value));
+
+      const url = `/api/property?${String(searchParams)}`;
       const res = await fetch(url);
 
       const { data } = (await res.json()) as ApiResponse<
         InmueblesAlquilerWithPropietary[]
       >;
       if (res.status === 200) {
+        isMaxElements.value = data.length < 10;
         if (data.length > 0) {
           properties.value = [...properties.value, ...data];
           skip.value += 10;
-        } else {
-          isMaxElements.value = true;
         }
       }
       isLoading.value = false;
@@ -47,7 +51,7 @@ export default function ListProperties(
   return (
     <div class="flex flex-col">
       <PropertyTable properties={properties} />
-      {!isMaxElements.value && (
+      {showButton.value && (
         <Button
           type="button"
           state="secondary"
