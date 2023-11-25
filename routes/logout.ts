b85fa10669ef877cf8a07da640aa78e61a88a55e
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-11-25 13:18:05 -0600

    fix(rh-26): Cerrar sesión como empleado te regresa al link de inicio de sesión de empleado

--- routes/logout.ts
+++ routes/logout.ts
@@ -1,20 +1,27 @@
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 import { deleteCookie } from "$cookies";
 import {
-  LOGOUT_URL,
+  ADMIN_LOGIN_URL,
+  LOGIN_URL,
   ROOT_URL,
   USER_SESSION_COOKIE_NAME,
 } from "@/utils/config.ts";
+import { Data } from "@/schema/data.ts";
+import SessionState from "@/schema/session-state.ts";
 
-export const handler: Handlers = {
-  GET(req: Request, _ctx: HandlerContext) {
+export const handler: Handlers<Data, SessionState> = {
+  GET(req: Request, ctx: HandlerContext<Data, SessionState>) {
     const headers = new Headers(req.headers);
 
     deleteCookie(headers, USER_SESSION_COOKIE_NAME, {
       path: ROOT_URL,
     });
 
-    headers.set("Location", LOGOUT_URL);
+    if (ctx.state.isEmployee) {
+      headers.set("Location", ADMIN_LOGIN_URL);
+    } else {
+      headers.set("Location", LOGIN_URL);
+    }
 
     return new Response(null, {
       status: 307,
