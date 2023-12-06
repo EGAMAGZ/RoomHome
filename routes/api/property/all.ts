c830f098f01ec30fa2d02d63dcbc4d3c9fcb3a14
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-12-06 11:13:48 -0600

    Terminado fix

--- routes/api/property/all.ts
+++ routes/api/property/all.ts
@@ -1,7 +1,6 @@
 import prismaClient from "@/database/prisma.ts";
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 
-// FIXME: CHECAR EL ENDPOINT PARA LAS RUTAS CON SKIP COMO SEARCHPARAM
 export const handler: Handlers = {
   async GET(_req: Request, _ctx: HandlerContext) {
     const properties = await prismaClient.inmueblesAlquiler.findMany();
