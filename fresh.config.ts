6a6b16a907e28cacf31a318bca4dc67d7e36476e
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-01-05 14:35:16 -0600

    fix: Arreglado tailwind

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+// import tailwind from "$fresh/plugins/tailwind.ts";
+import tailwindPlugin from "https://deno.land/x/fresh_tailwindcss/mod.ts";
+import tailwindConfig from "./tailwind.config.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [
+    tailwindPlugin({
+      mode: "development", // or 'production'
+      input: "./style.css", // Relative path to the running script
+      verbose: false,
+      tailwindConfig,
+    }),
+  ],
 });
