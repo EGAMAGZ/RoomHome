7bd464e11b74143eda306350cee7cad39d4ff5c5
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-01-04 20:13:45 -0600

    feat: Actualizacion de framework

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,15 @@
+import { type Config } from "tailwindcss";
 import daisyui from "daisyui";
-import tailwindForms from "@tailwindcss/forms";
-import tailwindTypography from "@tailwindcss/typography";
+import tailwindForms from "tailwindcss/forms";
+import tailwindTypography from "tailwindcss/typography";
 
 export default {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./islands/**/*.{js,ts,jsx,tsx,mdx}",
-    "./routes/**/*.{js,ts,jsx,tsx,mdx}",
+    "{routes,islands,components}/**/*.{ts,tsx}",
   ],
   plugins: [
     daisyui,
     tailwindForms,
     tailwindTypography,
   ],
-};
+} satisfies Config;
