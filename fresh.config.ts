e25b37c0258b5e45eb623d290c406e6e22437ec1
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-01-08 19:24:50 -0600

    fix: Configurado plugin de tailwind nativo

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "$fresh/server.ts";
-// import tailwind from "$fresh/plugins/tailwind.ts";
-import tailwindPlugin from "https://deno.land/x/fresh_tailwindcss/mod.ts";
-import tailwindConfig from "./tailwind.config.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
   plugins: [
-    // tailwindPlugin({
-    //   mode: "development", // or 'production'
-    //   input: "./style.css", // Relative path to the running script
-    //   verbose: false,
-    //   tailwindConfig,
-    // }),
+    tailwind(),
   ],
 });
